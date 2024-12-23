c6ddeb7a828eecd858fa2d69c0b64907829fa9ff
ThomasGysemans <gysemansthomas@gmail.com>
2024-12-23 12:13:55 +0100

    challenge 2 part 1

--- ts/src/challenge02/part1.ts
+++ ts/src/challenge02/part1.ts
@@ -1,3 +1,27 @@
+import { getInput } from "..";
+
 export default function code() {
+    const lines = getInput().split("\n");
+
+    let safeReports = 0;
     
+    main: for (let i = 0; i < lines.length; i++) {
+        const numbers = lines[i].split(" ").map(e => parseInt(e));
+        const isIncreasing = numbers[1] > numbers[0];
+        for (let j = 0; j < numbers.length - 1; j++) {
+            const delta = numbers[j] - numbers[j + 1];
+            if (isIncreasing) {
+                if (numbers[j] >= numbers[j + 1] || !(delta <= -1 && delta >= -3)) {
+                    continue main;
+                }
+            } else {
+                if (numbers[j] <= numbers[j + 1] || !(delta >= 1 && delta <= 3)) {
+                    continue main;
+                }
+            }
+        }
+        safeReports++;
+    }
+
+    return safeReports;
 }
--- ts/src/index.ts
+++ ts/src/index.ts
@@ -1,8 +1,13 @@
-import code from "./challenge01/part2";
+import code from "./challenge02/part1";
+import * as fs from "fs";
+
+export function getInput(): string {
+    return fs.readFileSync("./input.txt").toString();
+}
 
 console.time("Runtime");
 try {
-    console.log(code());
+    console.log(`Result is`, code());
 } finally {
     console.timeEnd("Runtime");
 }
